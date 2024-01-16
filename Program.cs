2bef4b6b5aaee04dba643cd39698560e6e5622b8
Lowrins <Lowrins@LAPTOP-RCL93GI4>
2024-01-16 23:54:03 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cc3_1b_antolinec_csharprampup2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
